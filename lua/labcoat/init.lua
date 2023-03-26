6acfd4a0a38d476baa5768821b43b05cf144ff09
Pierre Chevalier <pierrechevalier83@gmail.com>
2023-03-26 23:02:56 +0100

    fork: implement customizable palette

--- lua/labcoat/init.lua
+++ lua/labcoat/init.lua
@@ -8,9 +8,11 @@ local default_opts = {
     italic = true,
     italic_comments = false,
     minimal_mode = false,
-    alternate_backgrounds = false,
     custom_colors = function()
         return {}
+    end,
+    custom_palette = function()
+	return { palette }
     end,
 }
 
@@ -29,19 +31,6 @@ local style_names = {
     'underlineline',
 }
 
-local alternate_buffers_supported = {
-    'terminal',
-    'qf',
-    'vista_kind',
-    'packer',
-}
-
-local alternate_highlights_supported = {
-    'nvim-tree',
-    'telescope',
-    'whichkey',
-}
-
 local function create_options(config)
     local module_opts = {}
     local global_opts = {}
@@ -54,28 +43,7 @@ local function create_options(config)
     return vim.tbl_extend('force', default_opts, global_opts, module_opts)
 end
 
-local function create_alternatives(options)
-    local buffers = {}
-    local extensions = {}
-
-    local function filter(check)
-        return vim.tbl_filter(function(value)
-            return vim.tbl_contains(check, value)
-        end, options.alternate_backgrounds)
-    end
-
-    if options.alternate_backgrounds == true then
-        buffers = alternate_buffers_supported
-        extensions = alternate_highlights_supported
-    elseif type(options.alternate_backgrounds) == 'table' then
-        buffers = filter(alternate_buffers_supported)
-        extensions = filter(alternate_highlights_supported)
-    end
-
-    return { buffers = buffers, extensions = extensions }
-end
-
-local function create_arguments(options, alternatives)
+local function create_arguments(options)
     local s = {}
     for _, v in pairs(style_names) do
         s[v] = v
@@ -85,19 +53,15 @@ local function create_arguments(options, alternatives)
         underline = options.underline_option and s[options.underline_option] or s.none,
         italic = (options.italic == true or options.italic == nil) and s.italic or s.none,
         comments = options.italic_comments and s.italic or s.none,
-        bg = function(name)
-            local found = vim.tbl_contains(alternatives.extensions, name)
-            return found and palette.dark_black_alt or palette.dark_black
-        end,
     }
+    local chosen_palette = palette(options)
 
-    return { palette, s, cs, options }
+    return { chosen_palette, s, cs, options }
 end
 
 local function initialize(config)
     local options = create_options(config)
-    local alternatives = create_alternatives(options)
-    local arguments = create_arguments(options, alternatives)
+    local arguments = create_arguments(options)
     local groups = load_colors()
 
     if type(options.custom_colors) == 'function' then
@@ -146,14 +110,6 @@ local function initialize(config)
         vim.cmd('autocmd!')
         vim.cmd([[autocmd ColorScheme * lua require('labcoat').destroy()]])
 
-        for _, name in ipairs(alternatives.buffers) do
-            if name == 'terminal' then
-                vim.cmd('autocmd TermOpen * ' .. setlocal)
-            else
-                vim.cmd('autocmd FileType ' .. name .. ' ' .. setlocal)
-            end
-        end
-
         vim.cmd('augroup end')
     end
 
@@ -176,16 +132,16 @@ function M.colorscheme(config)
     vim.g.terminal_color_2 = palette.green
     vim.g.terminal_color_3 = palette.yellow
     vim.g.terminal_color_4 = palette.blue
-    vim.g.terminal_color_5 = palette.purple
-    vim.g.terminal_color_6 = palette.bright_cyan
+    vim.g.terminal_color_5 = palette.violet
+    vim.g.terminal_color_6 = palette.cyan
     vim.g.terminal_color_7 = palette.white
-    vim.g.terminal_color_8 = palette.gray
-    vim.g.terminal_color_9 = palette.red
-    vim.g.terminal_color_10 = palette.green
-    vim.g.terminal_color_11 = palette.yellow
-    vim.g.terminal_color_12 = palette.blue
-    vim.g.terminal_color_13 = palette.purple
-    vim.g.terminal_color_14 = palette.cyan
+    vim.g.terminal_color_8 = palette.bright_black
+    vim.g.terminal_color_9 = palette.bright_red
+    vim.g.terminal_color_10 = palette.bright_green
+    vim.g.terminal_color_11 = palette.bright_yellow
+    vim.g.terminal_color_12 = palette.bright_blue
+    vim.g.terminal_color_13 = palette.bright_violet
+    vim.g.terminal_color_14 = palette.bright_cyan
     vim.g.terminal_color_15 = palette.bright_white
 
     initialize(config or {})
--- lua/labcoat/palette.lua
+++ lua/labcoat/palette.lua
@@ -1,30 +1,78 @@
-return {
-    -- Polar Night
-    dark_black = '#2e3440', -- bg
-    black = '#3b4252',
-    bright_black = '#434c5e',
-    gray = '#4c566a',
-
-    -- Custom
-    darkish_black = '#3B414D',
-    grayish = '#667084',
-    dark_black_alt = '#2B303B',
-
-    -- Snow Storm
-    dark_white = '#d8dee9',
-    white = '#e5e9f0',
-    bright_white = '#eceff4',
-
-    -- Frost
-    cyan = '#8fbcbb', -- classes, types and primitives.
-    bright_cyan = '#88c0d0', -- declarations, calls and execution statements of functions, methods and routines.
-    blue = '#81a1c1', -- keywords, support characters, operators, tags, units, punctuations
-    intense_blue = '#5e81ac', -- pragmas, comment keywords and pre-processor statements.
-
-    -- Aurora
-    red = '#bf616a', -- deletions and errors
-    orange = '#d08770', -- annotations and decorators
-    yellow = '#ebcb8b', -- modifications, warning and escape characters
-    green = '#a3be8c', -- additions and string
-    purple = '#b48ead', -- integers and floating point numbers
-}
+return function(options)
+    return {
+	    -- Base colors
+	    base03 = options.base03 or "#002a35"
+	    base02 = options.base02 or "#083540"
+	    base01 = options.base01 or "#49707c"
+	    base00 = options.base00 or "#567d89"
+	    base0  = options.base0  or "#8b8372"
+	    base1  = options.base1  or "#98907f"
+	    base2  = options.base2  or "#f0e7d5"
+	    base3  = options.base3  or "#fff6e3"
+	    -- Accent colors
+	    blue = options.blue or "#4a89b8"
+	    violet = options.violet or "#6a74a9"
+	    magenta = options.magenta or "#b7577f"
+	    red = options.red or "#c35446"
+	    orange = options.orange or "#b85e39"
+	    yellow = options.yellow or "#b18a44"
+	    green = options.green or "#929546"
+	    cyan = options.cyan or "569c96"
+	    -- Bright accent colors
+	    bright_blue = options.bright_blue or "#19b5ff"
+	    bright_violet = options.bright_violet or "#8399f2"
+	    bright_magenta = options.bright_magenta or "#ff61ab"
+	    bright_red = options.bright_red or "#ff5a4b"
+	    bright_orange = options.bright_orange or "#ff6f32"
+	    bright_yellow = options.bright_yellow or "#eaaf31"
+	    bright_green = options.bright_green or "#b7c032"
+	    bright_cyan = options.bright_cyan or "#47ccc1"
+	    -- Base configuration
+	    if (options.dark_theme == true or options.dark_theme == nil) then
+		if (options.high_contrast == true or options.high_contrast == nil) then
+		    -- High contrast, Dark theme
+		    primary_bg = base03
+		    primary_fg = base0
+		    emphasized_bg = base02
+		    emphasized_fg = base1
+		else
+		    -- Low contrast, Dark theme
+		    primary_bg = base02
+		    primary_fg = base1
+		    emphasized_bg = base03
+		    emphasized_fg = base0
+		end
+		secondary_bg = base00
+		secondary_fg = base01
+		black = base00
+		bright_black = base01
+		white = base2
+		bright_white = base3
+	    else
+		if options.high_contrast == true then
+		    -- High contrast, Light theme
+		    primary_bg = base3
+		    primary_fg = base00
+		    emphasized_bg = base2
+		    emphasized_fg = base01
+		else
+		    -- Low contrast, Light theme
+		    primary_bg = base2
+		    primary_fg = base01
+		    emphasized_bg = base3
+		    emphasized_fg = base00
+		end
+		secondary_bg = base0
+		secondary_fg = base1
+		black = base02
+		bright_black = base03
+		white = base0
+		bright_white = base1
+	    end
+	    -- Accent configuration
+	    primary_accent = options.primary_accent or cyan
+	    primary_bright_accent = options.primary_bright_accent or bright_cyan
+	    secondary_accent = options.secondary_accent or blue
+	    secondary_bright_accent = options.secondary_bright_accent or bright_blue
+    }
+end
